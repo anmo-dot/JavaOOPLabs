d5bc727a1670312162e229f4b736d50f7e95f5ba
anmo-dot <82247315+anmo-dot@users.noreply.github.com>
2023-07-03 12:50:49 +0000

    Create Control.java

---
+++ assignments/day2/hw2/Control.java
@@ -0,0 +1,20 @@
+package hw2;
+
+public class Control {
+
+	public static void main(String[] args) {
+		boolean bool = true;
+		for(int i = 0; i <5; i++) {
+			for (int j = 0; j <10; j++) {
+				System.out.println(j+"\t");
+				if (j > 5) {
+					break;
+				}
+			}
+			System.out.println("Outer loop");
+		}
+		System.out.println("End");
+
+	}
+
+}
