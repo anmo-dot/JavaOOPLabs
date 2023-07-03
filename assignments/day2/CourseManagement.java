9f44be3cb1945ca38a64e5eca465065448b8a566
anmo-dot <82247315+anmo-dot@users.noreply.github.com>
2023-07-03 12:57:49 +0000

    Add files via upload

---
+++ assignments/day2/CourseManagement.java
@@ -0,0 +1,16 @@
+package hw7;
+
+public class CourseManagement {
+
+	public static void main(String[] args) {
+		Student student = new Student();
+		System.out.println(student.getStudentId());
+		System.out.println(student.getStudentType());
+		Student per = new Student();
+		per.setStudentId(105);
+		per.setStudentType('B');
+		System.out.println(per.getStudentId());
+		System.out.println(per.getStudentType());
+	}
+
+}
