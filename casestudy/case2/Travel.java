e5c410bd2a6c45b4eed957b2a8905c060bded863
anmo-dot <82247315+anmo-dot@users.noreply.github.com>
2023-07-03 12:43:42 +0000

    Create Travel.java

---
+++ casestudy/case2/Travel.java
@@ -0,0 +1,25 @@
+package case2;
+
+public class Travel {
+	char modeOfTravel;
+	int noOfTickets;
+	double amount;
+	public char getModeOfTravel() {
+		return modeOfTravel;
+	}
+	public void setModeOfTravel(char modeOfTravel) {
+		this.modeOfTravel = modeOfTravel;
+	}
+	public int getNoOfTickets() {
+		return noOfTickets;
+	}
+	public void setNoOfTickets(int noOfTickets) {
+		this.noOfTickets = noOfTickets;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+}
