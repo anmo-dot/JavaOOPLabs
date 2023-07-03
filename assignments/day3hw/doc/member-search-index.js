6672839359b55ecc85e0525e21e68edda33a8b3d
anmo-dot <82247315+anmo-dot@users.noreply.github.com>
2023-07-03 13:04:09 +0000

    Delete assignments/day3hw/doc directory

--- assignments/day3hw/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"HW5","c":"JavaDoc","l":"JavaDoc()","u":"%3Cinit%3E()"},{"p":"HW5","c":"JavaDoc","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
