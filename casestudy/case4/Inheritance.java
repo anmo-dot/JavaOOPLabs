98236b29c0c41fb51e10e62dd55d56e892e2234c
anmo-dot <82247315+anmo-dot@users.noreply.github.com>
2023-07-03 12:45:13 +0000

    Create Inheritance.java

---
+++ casestudy/case4/Inheritance.java
@@ -0,0 +1,18 @@
+package case4;
+
+public class Inheritance {
+	/*
+	 * Structure of Class:
+	 * 
+	 * Scenario 1: Inheritance applicable
+	 * 		Attribute: mode of travel, source, destination
+	 * 					date and time, ticket details, 
+	 * 					travel class type.
+	 * 		Sub Classes: Flight, Train, Bus
+	 * 
+	 * Scenario 2: Inheritance not applicable
+	 * 		OneWay Class Attributes: destination, km, total charge
+	 * 		OneDay Class Attributes: Start time, Bus number, locations, total charge
+	 * 		Package Class Attributes: Hotel, locations, schedule, total number of people
+	 * */
+}
